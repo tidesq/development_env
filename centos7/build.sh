ef6db81c5dbb7f460e1d89763b4b804d134a3bb3
tidesq <tidesq@tidesq-mac-pro18.local>
2018-12-06 20:52:38 +0800

    development envrionment dockerfile

---
+++ centos7/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+tag="0.1"
+image_name="centos7_devtoolset4"
+echo "${image_name}:${tag}"
+
+docker build  -f Dockerfile -t ${image_name}:${tag} .
+#docker build --net=host --pull=true --no-cache=true -f Dockerfile -t ${image_name}:${tag} .
